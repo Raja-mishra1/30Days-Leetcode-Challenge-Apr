e3d0b37ce392d4f4e003edef34f16102504fa5a5
RAJA MISHRA <rajamishra0702@gmail.com>
2020-04-30 22:10:46 +0530

    Delete Day12.py

--- Day12.py
+++
@@ -1,9 +0,0 @@
-import heapq
-class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
-        for i in range(len(stones) - 1):
-            stones.sort()
-            temp=stones[-1] - stones[-2]
-            stones[-2]=-1
-            stones[-1]=temp
-        return (stones[-1])
